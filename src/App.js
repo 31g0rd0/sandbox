dbe0f071ee4ec2107794a89795ebe719ed4dd212
gordo <hamster.doom@gmail.com>
2019-01-26 11:28:21 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React faster
+            Your mother Trebek!
           </a>
         </header>
       </div>
